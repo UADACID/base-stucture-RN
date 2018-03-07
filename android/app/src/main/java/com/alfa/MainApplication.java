d30ef9153cdde42bbe347350e5a67d5c6f22bcf1
Pratama <pratamasetya99@gmail.com>
2018-03-08 01:56:06 +0700

    add fast image, viewshot, imagepicker

--- android/app/src/main/java/com/alfa/MainApplication.java
+++ android/app/src/main/java/com/alfa/MainApplication.java
@@ -3,6 +3,9 @@ package com.alfa;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNViewShotPackage(),
+            new ImagePickerPackage(),
+            new FastImageViewPackage()
       );
     }
 
